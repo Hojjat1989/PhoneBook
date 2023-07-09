d504f1a3a929e6166c55cdca5f182a4ec96b5798
Hojjat Esmaeilzadeh <h.esmaeilzadeh1989@gmail.com>
2023-07-09 17:18:46 +0330

    add application layer

---
+++ PhoneBook.Application/Interfaces/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PhoneBook.Domain;
+
+namespace PhoneBook.Application.Interfaces;
+
+public interface IContactService
+{
+    Contact GetContactById(int id);
+    IEnumerable<Contact> GetContactsWithTagId(int tagId);
+    void CreateContact(Contact contact);
+    void UpdateContact(Contact contact);
+    void DeleteContact(int id);
+}
---
+++ PhoneBook.Application/Services/ContactService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PhoneBook.Application.Interfaces;
+using PhoneBook.Domain.Repositories;
+using PhoneBook.Domain;
+
+namespace PhoneBook.Application.Services;
+
+public class ContactService : IContactService
+{
+    private readonly IContactRepository _contactRepository;
+
+    public ContactService(IContactRepository contactRepository)
+    {
+        _contactRepository = contactRepository;
+    }
+
+    public Contact GetContactById(int id)
+    {
+        return _contactRepository.GetById(id);
+    }
+
+    public IEnumerable<Contact> GetContactsWithTagId(int tagId)
+    {
+        return _contactRepository.GetAllWithTagId(tagId);
+    }
+
+    public void CreateContact(Contact contact)
+    {
+        _contactRepository.Add(contact);
+    }
+
+    public void UpdateContact(Contact contact)
+    {
+        _contactRepository.Update(contact);
+    }
+
+    public void DeleteContact(int id)
+    {
+        _contactRepository.Delete(id);
+    }
+}
